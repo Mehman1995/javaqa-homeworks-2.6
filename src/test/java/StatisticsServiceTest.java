79401fee8ab4297952ad9e0d4b3303d99fc4c461
Mehman <mixailguliev@mail.ru>
2025-05-11 13:29:52 +0300

    initial Commit

---
+++ src/test/java/StatisticsServiceTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class StatisticsServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void myFindMax(){
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {1, 4, 22, 5, 77, 8, 33, 16, 15, 19, 14};
+        long expected = 77;
+
+        long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected,actual);
+    }
+
+}
